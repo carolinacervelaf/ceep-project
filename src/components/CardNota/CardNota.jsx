e3ab31fdfea3fc16529b92cb1f4583d596130153
Carolina Freire <carolinacervelaf@gmail.com>
2022-01-06 20:45:40 -0300

    adicionando o delete dos cards

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import "./estilo.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
+
+  apagar(){
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+
   render() {
     return (
       <section className='card-nota'>
         <header className='car-nota_cabecalho'>
-          <h3 className='card-noa_titulo'>{this.props.titulo}</h3>
+          <div className='div-delete-titulo'>
+          <DeleteSVG className='img-svg-delete' onClick={this.apagar.bind(this)}/>
+          <h3 className='card-nota_titulo'>{this.props.titulo}</h3>
+          </div>
         </header>
         <p className='car-nota_texto'>{this.props.texto}</p>
       </section>
